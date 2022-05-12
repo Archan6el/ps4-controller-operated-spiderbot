9fc68b5370056755f135ea1fd996b5e55cb53ae1
Archan6el <91164464+Archan6el@users.noreply.github.com>
2022-05-11 21:05:22 -0500

    Update onBoot.sh

--- onBoot.sh
+++ onBoot.sh
@@ -5,3 +5,5 @@ sleep 5
 sudo systemctl restart bluetooth
 sleep 3
 sudo python3 adeept_darkpaw/server/ps4.py
+#bluetooth is restarted multiple times on boot because connecting to the bluetooth PS4 controller is very finnicky. A restart will usually do the trick but not always
+#A double restart is used just in case
